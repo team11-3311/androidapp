664500b419542b0196ed6a970ea128d991f2a6f6
geegeechic <32759528+geegeechic@users.noreply.github.com>
2019-04-08 01:32:05 -0500

    updated files

---
+++ Dontdestroy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dontdestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static Dontdestroy tref;
+    void Awake()
+    {
+        //DontDestroyOnLoad(gameObject);
+        if (tref == null)
+        {
+            tref = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        /* if (gameObject.activeSelf == false)
+         {
+             //gameObject.SetActive(false);
+             //Debug.Log("destroyed");
+
+             if (FindObjectsOfType(GetType()).Length > 1)
+             {
+                 Destroy(gameObject);
+             }
+         }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GetAnswer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class GetAnswer : MonoBehaviour
+{
+    // Start is called before the first frame update]
+    public GameObject rad;
+    
+    void Start()
+    { var input = gameObject.GetComponent<InputField>();
+        var answer = new InputField.SubmitEvent();
+        
+        answer.AddListener(SubmitName);
+        input.onEndEdit = answer;
+        //myText.onEndEdit.AddListener(SubmitName);
+        rad = GameObject.FindGameObjectWithTag("BOX");
+    }
+
+    private void SubmitName(string arg0)
+    {
+        //throw new NotImplementedException();
+        Debug.Log(arg0);
+        if(arg0.Equals("shoe"))
+        {
+            Debug.Log("right answer");
+            rad.SetActive(false);
+            if (EditorUtility.DisplayDialog("Success!!!", "You earned a token", "Proceed"))
+            {
+                SceneManager.LoadScene(3);
+            }
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
