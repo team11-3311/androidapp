06c42a2760c2a5fc93bcc9bfa8720187544464fc
geegeechic <32759528+geegeechic@users.noreply.github.com>
2019-04-08 12:45:06 -0500

    updates

--- MovePupil.cs
+++ MovePupil.cs
@@ -9,12 +9,9 @@ public class MovePupil : MonoBehaviour
     private Rigidbody2D rbd;
     public int speed;
     private Animator anim;
-   // private float walk = 0f;
-    //private CollisionFlags m_CollisionFlags;
-    //bool isCollision = false;
-    //string text = "YOU HIT ME";
-    //float wide = 0f;
-    //float high = 0f;
+    private CollisionFlags m_CollisionFlags;
+    bool isCollision = false;
+
     // Use this for initialization
     void Start()
     {
@@ -27,7 +24,6 @@ public class MovePupil : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //walk = Input.GetAxis("Horizontal");
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             
@@ -53,7 +49,7 @@ public class MovePupil : MonoBehaviour
         }
     }
 
-   /* private void OnControllerColliderHit(ControllerColliderHit hit)
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Rigidbody body = hit.collider.attachedRigidbody;
         //dont move the rigidbody if the character is on top of it
@@ -67,33 +63,26 @@ public class MovePupil : MonoBehaviour
             return;
         }
         body.AddForceAtPosition(rbd.velocity * 0.1f, hit.point, ForceMode.Impulse);
-    }*/
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        //Check the provided Collider2D parameter other to see if it is tagged, if it is...
         if (other.gameObject.CompareTag("BOX"))
         {
             Debug.Log("PUPIL collided with BOX");
             SceneManager.LoadScene(4);
-            //isCollision = true;
+            isCollision = true;
             //other.gameObject.SetActive(false);
         }
         if (other.gameObject.CompareTag("RADIO"))
         {
             Debug.Log("PUPIL collided with RADIO");
             SceneManager.LoadScene(5);
-            //isCollision = true;
+            isCollision = true;
             //other.gameObject.SetActive(false);
         }
     }
 
-    /*void OnGUI()
-    {
-        if (isCollision)
-        {
-            //GUI.TextArea(new Rect((Screen.width / 2) - wide, (Screen.height / 2) - high, (wide * 2), (high * 2)), text);
-            GUI.TextArea(new Rect(260, 60, 400, 300), text);
-        }
-    }*/
+
 }
