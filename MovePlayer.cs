33e5970108e41ebb1f5d3d3d7d805d89dd53e5e3
team11-3311 <47309700+team11-3311@users.noreply.github.com>
2019-04-29 11:25:30 -0500

    Add files via upload

---
+++ MovePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]Transform target;
+    [SerializeField]Direction dir; 
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        Vector3 pos = target.position;
+
+        switch(dir)
+        {
+            case Direction.DOWN:
+                pos += Vector3.down;
+                break;
+            case Direction.UP:
+                pos += Vector3.up;
+                break;
+            case Direction.LEFT:
+                pos += Vector3.left;
+                break;
+            case Direction.RIGHT:
+                pos += Vector3.right;
+                break;
+            default:
+                pos = Vector3.zero;
+                break;
+
+
+        }
+
+        target.position = pos;
+    }
+
+    
+}
+
+public enum Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
